f63d52e07e989140ee0759bbfeb6f425263f1af4
Bellco <bellco579@gmail.com>
2018-12-05 21:43:41 +0500

    dell files

--- client/BackEnd/Ready_to_Send.py
+++
@@ -1,7 +0,0 @@
-
-class Ready_to_Send():
-	def __init__(self, client_socket):
-		super(Ready_to_Send, self).__init__()
-		self.client_socket  = client_socket		
-	def send(self,data):
-		self.client_socket.send(b'test')
